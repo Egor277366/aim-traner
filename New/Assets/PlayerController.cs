fc4cbe902b72e7206dd3bbf5554a4a61e61ec6f3
EgorMyagkov <egormyagkov89@gmail.com>
2021-02-25 16:28:38 +0300

    дабавил камеру, теперь все работает

--- New/Assets/PlayerController.cs
+++ New/Assets/PlayerController.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X"));
     }
 }
