ce6dbd6bd6cf885b99ee7ae7843d2c60f7b9649b
EgorMyagkov <egormyagkov89@gmail.com>
2021-03-04 16:27:28 +0300

    сделал камеру

--- New/Assets/PlayerController.cs
+++ New/Assets/PlayerController.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Transform cameraHolder;
+    float verticalLookRotation;
+
+
     void Update()
     {
         transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X"));
+
+        verticalLookRotation -= Input.GetAxisRaw("Mouse Y");
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+        cameraHolder.localEulerAngles = new Vector3(verticalLookRotation, 0, 0);
     }
 }
