4fb7218c6bcd9b5827bdcb13de9ea2dff6adbf08
Egor277366 <77445392+Egor277366@users.noreply.github.com>
2021-03-05 16:33:05 +0300

    6.40+-

--- New/Assets/PlayerController.cs
+++ New/Assets/PlayerController.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    [SerializeField] Transform cameraHolder;
-    float verticalLookRotation;
-
-
-    void Update()
-    {
-        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X"));
-
+using UnityEngine;  
+ 
+public class PlayerController : MonoBehaviour 
+{ 
+    [SerializeField] Transform cameraHolder; 
+    float verticalLookRotation; 
+    float horisontalLookRotation; 
+      
+ 
+    void Update() 
+    { 
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X")); 
+         
         verticalLookRotation -= Input.GetAxisRaw("Mouse Y");
+        horisontalLookRotation += Input.GetAxisRaw("Mouse X"); 
         verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
-        cameraHolder.localEulerAngles = new Vector3(verticalLookRotation, 0, 0);
-    }
-}
+        horisontalLookRotation = Mathf.Clamp(horisontalLookRotation, -90f, 90f); 
+        cameraHolder.localEulerAngles = new Vector3(verticalLookRotation, horisontalLookRotation, 0); 
+    } 
+} 
