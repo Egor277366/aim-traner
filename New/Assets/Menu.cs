cede7a9338e392344b2360771c09277cfd0f6278
EgorMyagkov <egormyagkov89@gmail.com>
2021-04-29 16:29:40 +0300

    9.44

--- New/Assets/Menu.cs
+++ New/Assets/Menu.cs
@@ -1,18 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ScreneManagement;
 
 public class Menu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+    
+    public void PlayGame
     {
-        
+    ScreneManagement.LoadScrene(ScreneManager.GetActiveScrene().buildIndex +1);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
